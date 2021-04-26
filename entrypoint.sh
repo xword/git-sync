8a32497fdfda2dbcd18679cd445ac3d25511f759
Kamil Wojcik <kamil.wojcik@crosswordcybersecurity.com>
2021-04-26 14:38:14 +0200

    remove debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,19 @@
 
 set -e
 
+mkdir -p /root/.ssh
+
 if [[ -n "$SSH_PRIVATE_KEY" ]]; then
-  mkdir -p /root/.ssh
   echo "$SSH_PRIVATE_KEY" | sed 's/\\n/\n/g' >/root/.ssh/id_rsa
   chmod 600 /root/.ssh/id_rsa
 fi
 
 if [[ -n "$SOURCE_SSH_PRIVATE_KEY" ]]; then
-  mkdir -p /root/.ssh
   echo "$SOURCE_SSH_PRIVATE_KEY" | sed 's/\\n/\n/g' >/root/.ssh/src_rsa
   chmod 600 /root/.ssh/src_rsa
 fi
 
 if [[ -n "$DESTINATION_SSH_PRIVATE_KEY" ]]; then
-  mkdir -p /root/.ssh
   echo "$DESTINATION_SSH_PRIVATE_KEY" | sed 's/\\n/\n/g' >/root/.ssh/dst_rsa
   chmod 600 /root/.ssh/dst_rsa
 fi
--- git-sync.sh
+++ git-sync.sh
@@ -30,7 +30,7 @@ echo "DESTINATION=$DESTINATION_REPO:$DESTINATION_BRANCH"
 
 if [[ -n "$SOURCE_SSH_PRIVATE_KEY" ]]; then
   # Clone using source ssh key if provided
-  git clone -c core.sshCommand="/usr/bin/ssh -v -i ~/.ssh/src_rsa" "$SOURCE_REPO" /root/source --origin source && cd /root/source
+  git clone -c core.sshCommand="/usr/bin/ssh -i ~/.ssh/src_rsa" "$SOURCE_REPO" /root/source --origin source && cd /root/source
 else
   git clone "$SOURCE_REPO" /root/source --origin source && cd /root/source
 fi
@@ -43,11 +43,9 @@ git fetch source '+refs/heads/*:refs/heads/*' --update-head-ok
 # Print out all branches
 git --no-pager branch -a -vv
 
-ls -al ~/.ssh/
-
 if [[ -n "$DESTINATION_SSH_PRIVATE_KEY" ]]; then
   # Push using destination ssh key if provided
-  git config --local core.sshCommand "/usr/bin/ssh -v -i ~/.ssh/dst_rsa"
+  git config --local core.sshCommand "/usr/bin/ssh -i ~/.ssh/dst_rsa"
 fi
 
 git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" -f
